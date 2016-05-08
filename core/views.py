10598201e8a3a4aa3adb4c26959c611f8f821602
Stefan Mayer <stefanmayer13@gmail.com>
2016-05-08 21:52:01 +0200

    Added authentication

--- core/views.py
+++ core/views.py
@@ -1,20 +1,39 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.template import loader
-from django.views import generic
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    user = None
+    if request.user.is_authenticated():
+        user = request.user
+    return render(request, 'core/index.html', {
+        'user': user
+    })
 
 def login(request):
     if request.method == 'GET':
         return render(request, 'core/login.html')
     elif request.method == 'POST':
+        errorMessage = None
         username = request.POST['username']
         password = request.POST['password']
         if not (username and password):
-            return render(request, 'core/login.html', {
-                'error_message': "Please enter username and password.",
-            })
-        return HttpResponseRedirect(reverse('login'))
+            errorMessage = "Please enter username and password."
+        else:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return HttpResponseRedirect(reverse('core:index'))
+                else:
+                    errorMessage = "This user account ist not active."
+            else:
+                errorMessage = "Username or password wrong."
+        return render(request, 'core/login.html', {
+            'error_message': errorMessage,
+        })
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('core:index'))
